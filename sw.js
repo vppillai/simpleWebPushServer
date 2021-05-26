89581ce9c1696e64e018e7c7b3dcdbf668053081
Vysakh P Pillai <3634378+vppillai@users.noreply.github.com>
2021-05-26 12:40:08 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 self.addEventListener('push', (e) => {
     const data = e.data.json();
-    self.registration.showNotification(data.title,  {body: data.body, requireInteraction: true, icon:'https://localhost:4443/favicon.ico'}   );
+    self.registration.showNotification(data.title,  {body: data.body, requireInteraction: true, icon:'https://localhost:4443/favicon.ico', vibrate: [200, 100, 200, 100, 200, 100, 200]}   );
 });
